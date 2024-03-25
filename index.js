2c41842750c2d89a15c5d87480bf63c210f2d90c
SpriteMisaka <SpriteMisaka@gmail.com>
2024-03-26 02:10:20 +0800

    async

--- index.js
+++ index.js
@@ -18,16 +18,20 @@ hexo.extend.filter.register('before_post_render', function(data) {
         const uuid = crypto.randomUUID()
         code = replaceEscape(match[2])
         str = `<div style="display:flex;align-items:center;">\n` +
-            `<div><button id="coliru${uuid}" style="vertical-align:middle;">Coliru</button></div>\n` +
-            `<div><input id="results${uuid}" style="vertical-align:middle;margin-left:5px;" type="text" value="" readonly></div>\n` +
+            `<div><button id="coliru-${uuid}" style="vertical-align:middle;">Coliru</button></div>\n` +
+            `<div><input id="results-${uuid}" style="vertical-align:middle;margin-left:5px;" type="text" value="" readonly></div>\n` +
         `</div>\n` +
         match[1] + `\n` +
         `<script type="module">\n` +
-            `document.getElementById("coliru${uuid}").addEventListener("click", function() {\n` +
+            `document.getElementById("coliru-${uuid}").addEventListener("click", function() {\n` +
                 `var http = new XMLHttpRequest();\n` +
-                `http.open("POST", "http://coliru.stacked-crooked.com/compile", false);\n` +
+                `http.open("POST", "http://coliru.stacked-crooked.com/compile", true);\n` +
+                `http.onload = function (e) {\n` +
+                    `if (http.readyState === 4 && http.status === 200) {\n` +
+                        `document.getElementById("results-${uuid}").value = String(http.response);\n` +
+                    `}\n` +
+                `};` +
                 `http.send(JSON.stringify({ "cmd": "g++ main.cpp && ./a.out", "src": "` + code + `"}));\n` +
-                `document.getElementById("results${uuid}").value = String(http.response);\n` +
             `});\n` +
         `</script>\n`
         return str
